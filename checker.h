5b5951b7b700f070d08efb116bd167fffd2a2298
Matt Raymond <m4tt.r4ymond@gmail.com>
2019-03-19 01:37:23 +0000

    finished programming

--- checker.h
+++ checker.h
@@ -2,17 +2,21 @@
 
 #include <iostream>
 #include <string>
+#include <fstream>
+
 using namespace std;
 
 class checker
 {
 public:
-    static void cycle(string testString);
+    static bool cycle(ifstream& currentStream);
 private:
     static bool forward(char currentChar);
     static bool reverse(char currentChar);
     static char currentChar(char currentChar);
     static char compliment(char currentChar);
+    static void message(char charE, char charR, int lineNum);
+
 };
 
 #endif
