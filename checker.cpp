6f3f7c0c4a32b68b776efe7187e168f1f4e86ca3
Matt Raymond <m4tt.r4ymond@gmail.com>
2019-03-21 02:16:22 +0000

    Added comments and finished programming

--- checker.cpp
+++ checker.cpp
@@ -3,32 +3,27 @@
 #include <fstream>
 
 #include "checker.h"
-#include "stack.h"
+#include "GenStack.h"
 
 using namespace std;
 
-bool checker::forward(char currentChar)
-{
+// Takes in a character and tells if it's a forward delimiter, returns a bool
+bool checker::forward(char currentChar){
+    // Open delimiters
     char openD[] = {'(', '{', '['};
-    for(int i=0; i < 3; ++i)
-        if(currentChar == openD[i])
-            return true;
-    return false;
+    return boolCheck(currentChar, openD);
 }
 
-bool checker::reverse(char currentChar)
-{
+// Takes in a character and tells if it's a reverse delimiter, returns a bool
+bool checker::reverse(char currentChar){
+    // Closed delimeters
     char closeD[] = {')', '}', ']'};
-    for(int i=0; i < 3; ++i)
-        if(currentChar == closeD[i])
-            return true;
-    return false;
+    return boolCheck(currentChar, closeD);
 }
 
-char checker::compliment(char currentChar)
-{
-    switch (currentChar)
-    {
+// Takes in a character and returns the compliment of that character if it's a delimiter
+char checker::compliment(char currentChar){
+    switch (currentChar){
         case '(':
             return ')';
         case ')':
@@ -45,61 +40,54 @@ char checker::compliment(char currentChar)
     return currentChar;
 }
 
-bool checker::cycle(ifstream& currentStream)
-{
+// Takes in a filestream, checks for errors, and returns a bool for the error/lack thereof
+bool checker::cycle(ifstream& currentStream){
     int lineNumber = 1;
-    int current = -1; // Allows for preincrementing
-    Stack<char> mainStack;
+    // Where delimiters will be stored
+    GenStack<char> mainStack;
+    // Character being evaluated
     char currentInputChar;
-    char tempStackChar;
     bool error = false;
 
-    while(currentStream.good() && !error)
-    {
+    while(!error){
         currentInputChar = currentStream.get();
-
         if(currentInputChar == '\n') lineNumber++;
 
         // Adds to stack if opening
         if(forward(currentInputChar))
             mainStack.push(currentInputChar);
         // Checks if closing
-        else if(reverse(currentInputChar))
-        {
-            // Checks to make sure there aren't too many closing delimiters
-            if(mainStack.isEmpty())
-            {
+        else if(reverse(currentInputChar)){
+            // Checks to make sure there aren't more closing than opening
+            if(mainStack.isEmpty()){
                 message(EOF, currentInputChar, lineNumber);
-                error=true;
+                error = true;
             }
             // Removes from stack if correct closing
             else if(mainStack.peek() == compliment(currentInputChar))
                 mainStack.pop();
             // If it's the wrong closing delimiter
-            else
-            {
-                message(compliment(mainStack.peek()), currentInputChar, lineNumber); // wrong closing delimiter
-                error=true;
+            else{
+                message(compliment(mainStack.peek()), currentInputChar, lineNumber);
+                error = true;
             }
         }
         // Checks to make sure that there aren't too many opening delimiters
-        else if((currentInputChar == EOF)&&(mainStack.top>(-1)))
-        {
-            message(compliment(mainStack.peek()), currentInputChar, lineNumber); // too many opening
-            error=true;
+        else if((currentInputChar == EOF)&&(mainStack.top>(-1))){
+            message(compliment(mainStack.peek()), currentInputChar, lineNumber);
+            error = true;
         }
     }
     currentStream.close();
-    if(!error)
-    {
+    if(!error){
         cout << "No errors" << endl;
         return true;
     }
     return false;
 }
 
-void checker::message(char charE, char charR, int lineNum)
-{
+// Takes in an expected character, a received character, and the line number and prints the error message
+void checker::message(char charE, char charR, int lineNum){
     string expected = "EOF";
     string received = "EOF";
 
@@ -107,4 +95,12 @@ void checker::message(char charE, char charR, int lineNum)
     if(charR!=EOF) received = charR;
 
     cout << "Was expecting \"" << expected << "\", received \"" << received << "\" instead. Line " << lineNum << endl;
+}
+
+// Takes in a character and an array of characters and checks to see if the character is in the array
+bool checker::boolCheck(char currentChar, char *cA){
+    for(int i=0; i < 3; ++i)
+        if(currentChar == cA[i])
+            return true;
+    return false;
 }
