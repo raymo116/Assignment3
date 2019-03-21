4c1905a6ea3ac91d1e4513d4aed4b1d1a6e8f4bc
Matt Raymond <m4tt.r4ymond@gmail.com>
2019-03-21 02:16:39 +0000

    Added comments and finished programming

--- checker.h
+++ checker.h
@@ -9,14 +9,19 @@ using namespace std;
 class checker
 {
 public:
+    // Takes in a filestream, checks for errors, and returns a bool for the error/lack thereof
     static bool cycle(ifstream& currentStream);
 private:
+    // Takes in a character and tells if it's a forward delimiter, returns a bool
     static bool forward(char currentChar);
+    // Takes in a character and tells if it's a reverse delimiter, returns a bool
     static bool reverse(char currentChar);
-    static char currentChar(char currentChar);
+    // Takes in a character and returns the compliment of that character if it's a delimiter
     static char compliment(char currentChar);
+    // Takes in an expected character, a received character, and the line number and prints the error message
     static void message(char charE, char charR, int lineNum);
-
+    // Takes in a character and an array of characters and checks to see if the character is in the array
+    static bool boolCheck(char currentChar, char *cA);
 };
 
 #endif
